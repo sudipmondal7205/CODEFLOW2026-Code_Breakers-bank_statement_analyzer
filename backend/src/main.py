74c4517f40accab48d13a9fa57de12dd84c845ef
Kanchan-Baran-Hazra <kanchanbhazra7063@gmail.com>
2026-05-24 11:37:16 +0530

    add user service

--- backend/src/main.py
+++ backend/src/main.py
@@ -9,6 +9,7 @@ from src.services.ai_advisor import generate_ai_insights
 from starlette.middleware.sessions import SessionMiddleware
 from src.core.database import init_db
 import os
+from src.api.profile import router_profile
 
 load_dotenv()
 
@@ -27,4 +28,5 @@ app.add_middleware(
 
 app.include_router(auth_routes.router, prefix="/api/auth", tags=["auth"])
 app.include_router(statement_routes.router, prefix="/api/statements", tags=["statements"])
+app.include_router(router_profile, prefix="/api/profile", tags=["profile"])
 
